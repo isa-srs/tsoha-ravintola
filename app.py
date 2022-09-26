231599286d2ed5d36ecdf4fc383c580207472579
isabel <isabel.suorsa@helsinki.fi>
2022-09-27 01:17:42 +0300

    Fix app.py

--- app.py
+++ app.py
@@ -1,23 +1,5 @@
 from flask import Flask
-from flask import redirect, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.form["username"]
-    password = request.form["password"]
-    # session["username"] = username
-    return redirect("/logintest")
-
-@app.route("/login", methods=["GET"])
-def login2():
-    return render_template("login.html")
-
-@app.route("/logintest", methods=["POST"])
-def user():
-    return render_template("logintest.html", name=request.form["username"])
+import routes
