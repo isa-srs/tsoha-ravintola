1da7aad9dc973a462615cece430abad5f04e086c
isabel <isabel.suorsa@helsinki.fi>
2022-09-27 13:46:15 +0300

    Changes in users.py and add check_csrf

--- users.py
+++ users.py
@@ -1,14 +1,37 @@
 import os
 from db import db
-from flask import session
-from werkzeug.security import generate_password_hash
+from flask import session, abort, request
+from werkzeug.security import generate_password_hash, check_password_hash
+
+def login(username, password):
+    sql = "SELECT id, password, role FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    if not check_password_hash(user[1], password):
+        return False
+    session["user_id"] = user[0]
+    session["user_name"] = username
+    session["user_role"] = user[2]
+    session["csrf_token"] = os.urandom(16).hex()
+    return True
+
+def logout():
+    del session["user_id"]
+    del session["user_name"]
+    del session["user_role"]
 
 def register(username, password, role):
     hash_value = generate_password_hash(password)
     try:
-        sql = "INSERT INTO users (username, password, role) VALUES (:name, :password, :role)"
-        db.session.execute(sql, {"name":username, "password":hash_value, "role":role})
+        sql = "INSERT INTO users (username, password, role) VALUES (:username, :password, :role)"
+        db.session.execute(sql, {"username":username, "password":hash_value, "role":role})
         db.session.commit()
     except:
         return False
-    return True
+    return login(username, password)
+
+def check_csrf():
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
