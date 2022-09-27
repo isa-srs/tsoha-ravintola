6b91b6de5edbd01f51582c44fc1371bf92086c6c
isabel <isabel.suorsa@helsinki.fi>
2022-09-27 13:44:37 +0300

    sql commands and add logout

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, request, redirect
+import users
 
 @app.route("/")
 def index():
@@ -9,23 +10,24 @@ def index():
 def login():
     if request.method == "GET":
         return render_template("login.html")
-    
     else:
         username = request.form["username"]
         password = request.form["password"]
-        return redirect("/logintest")
+        if not users.login(username, password):
+            return render_template("error.html", message="Väärä käyttäjänimi tai salasana")
+        return redirect("/")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
 
 @app.route("/register", methods=["get", "post"])
 def register():
     if request.method == "GET":
         return render_template("register.html")
-    
     else:
         username = request.form["username"]
         password = request.form["password"]
         role = request.form["role"]
-        return redirect("/logintest")
-
-@app.route("/logintest", methods=["post"])
-def user():
-    return render_template("logintest.html", name=request.form["username"])
+        return redirect("/")
