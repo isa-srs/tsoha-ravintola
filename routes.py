bec0034a463c9e405332042879fa1b6150d7e7bf
isabel <isabel.suorsa@helsinki.fi>
2022-09-28 13:29:04 +0300

    Fix login and register

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
 import users
 
 @app.route("/")
@@ -10,11 +10,13 @@ def index():
 def login():
     if request.method == "GET":
         return render_template("login.html")
-    else:
+
+    elif request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
         if not users.login(username, password):
             return render_template("error.html", message="Väärä käyttäjänimi tai salasana")
+        session["username"] = username
         return redirect("/")
 
 @app.route("/logout")
@@ -26,8 +28,17 @@ def logout():
 def register():
     if request.method == "GET":
         return render_template("register.html")
-    else:
+    
+    if request.method == "POST":
         username = request.form["username"]
+        if len(username) < 4 or len(username) > 15:
+            return render_template("error.html", message="Käyttäjänimen tulee olla 4-15 merkkiä pitkä.")
+        
         password = request.form["password"]
+        if len(password) < 6:
+            return render_template("error.html", message="Salasana liian lyhyt. Salasanassa tulee olla vähintään 6 merkkiä.")
+        
         role = request.form["role"]
+        if not users.register(username, password, role):
+            return render_template("error.html", message="Rekisteröinti epäonnistui.")
         return redirect("/")
--- users.py
+++ users.py
@@ -4,23 +4,25 @@ from flask import session, abort, request
 from werkzeug.security import generate_password_hash, check_password_hash
 
 def login(username, password):
-    sql = "SELECT id, password, role FROM users WHERE username=:username"
+    sql = "SELECT id, username, password, role FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
+    print(user)
     if not user:
         return False
-    if not check_password_hash(user[1], password):
+    if not check_password_hash(user[2], password):
         return False
-    session["user_id"] = user[0]
-    session["user_name"] = username
-    session["user_role"] = user[2]
+    session["user_id"] = user.id
+    session["username"] = user.username
+    session["userrole"] = user.role
     session["csrf_token"] = os.urandom(16).hex()
     return True
 
 def logout():
     del session["user_id"]
-    del session["user_name"]
-    del session["user_role"]
+    del session["username"]
+    del session["userrole"]
+    del session["csrf_token"]
 
 def register(username, password, role):
     hash_value = generate_password_hash(password)
