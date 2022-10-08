736fc331b722d7e6684b33b3340c14d6d228d0c6
isabel <isabel.suorsa@helsinki.fi>
2022-10-08 15:34:27 +0300

    update schema

--- schema.sql
+++ schema.sql
@@ -9,5 +9,32 @@ CREATE TABLE restaurants (
     id SERIAL PRIMARY KEY,
     owner_id INTEGER REFERENCES users,
     name TEXT,
-    cuisine TEXT
+    descr TEXT
+);
+
+CREATE TABLE schedules (
+    id SERIAL PRIMARY KEY,
+    restaurant_id INTEGER REFERENCES restaurants,
+    opens_at TEXT,
+    closes_at TEXT
+);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    stars INTEGER,
+    comment TEXT,
+    posted_at TIMESTAMP DEFAULT Now()
+);
+
+CREATE TABLE cuisines (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    restaurant_id INTEGER REFERENCES restaurants
+);
+
+CREATE TABLE favourites (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    restaurant_id INTEGER REFERENCES restaurants
 );
